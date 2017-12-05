a2ba71a4cfeb5019d5b510baf0cf29ac4c4626be
shaban-abd-al-shafy <agriengshaban@gmail.com>
2017-12-05 17:48:13 +0300

    last work on project : deleverd

--- js/custom.js
+++ js/custom.js
@@ -16,5 +16,36 @@
 /*start scroll top button*/
 
 
+$('#live-chat .chat-header').click(function () {
+  $('.chat').slideToggle();
+});
+
+//$(".chat").delay(5000).slideToggle();
+
+setTimeout(function(){
+  $('.chat').css({
+    display: "block",
+    opacity: "1"
+  });
+}, 6000);
+
+$(document).ready(function () {
+    "use strict";
+    $(".comments-text-apear").hide();
+    
+    $(".comment .down").click(function () {
+        $(".comments-text-apear").fadeIn();
+        $(".comment .down").hide();
+        $(".comment .up").show();
+        /*$(".subscribe .form-inline .col-sm-8").removeClass('comment-limit');*/
+    });
+    
+    $(".comment .up").click(function () {
+        $(".comments-text-apear").fadeOut();
+        $(".comment .down").show();
+        $(".comment .up").hide();
+        /*$(".subscribe .form-inline .col-sm-8").removeClass('comment-limit');*/
+    });
+});
 
 
